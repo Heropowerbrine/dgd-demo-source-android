374061454f0c3bb2a0e0f384dcb973bbf9ed9f37
Hero <78842166+Heropowerbrine@users.noreply.github.com>
2022-04-16 19:09:58 +0800

    Update Paths.hx

--- source/Paths.hx
+++ source/Paths.hx
@@ -140,7 +140,7 @@ class Paths
 		var imageToReturn:FlxGraphic = addCustomGraphic(key);
 		if(imageToReturn != null) return imageToReturn;
 		#end
-		return ('images/$key.png', IMAGE, library);
+		return getPath('images/$key.png', IMAGE, library);
 	}
 	
 	static public function getTextFromFile(key:String, ?ignoreMods:Bool = false):String
