5c81b0b49505ebdba2eb00b13bde05c46c05d2c3
Hero <78842166+Heropowerbrine@users.noreply.github.com>
2022-04-20 17:22:24 +0800

    gonna do video code now(have not checked controls)

--- source/MP4Handler.hx
+++ source/MP4Handler.hx
@@ -1,75 +1,84 @@
-package;
-
-import flixel.FlxG;
-import flixel.FlxSprite;
-import flixel.FlxState;
-import flixel.util.FlxColor;
-import flixel.util.FlxTimer;
+#if web
+import openfl.net.NetConnection;
+import openfl.net.NetStream;
+import openfl.events.NetStatusEvent;
+import openfl.media.Video;
+#elseif android
+import extension.videoview.VideoView;
+import android.AndroidTools;
+#else
 import openfl.events.Event;
 import vlc.VlcBitmap;
+#end
+import flixel.FlxBasic;
+import flixel.FlxG;
 
-// THIS IS FOR TESTING
-// DONT STEAL MY CODE >:(
-class MP4Handler
-{       #if desktop
-	public var finishCallback:Void->Void;
-	public var stateCallback:FlxState;
+class FlxVideo extends FlxBasic {
+	public var finishCallback:Void->Void = null;
+	
+	#if desktop
+	public static var vlcBitmap:VlcBitmap;
+	#end
 
-        #if desktop
-	public var bitmap:VlcBitmap;
-        #end
+	public function new(name:String) {
+		super();
 
-	public var sprite:FlxSprite;
-
-	public function new()
-	{
-		FlxG.autoPause = false;
-	}
-
-	public function playMP4(path:String, ?repeat:Bool = false, ?outputTo:FlxSprite = null, ?isWindow:Bool = false, ?isFullscreen:Bool = false,
-			?midSong:Bool = false):Void
-	{
-		if (!midSong)
-		{
-			if (FlxG.sound.music != null)
-			{
-				FlxG.sound.music.stop();
+		#if web
+		var player:Video = new Video();
+		player.x = 0;
+		player.y = 0;
+		FlxG.addChildBelowMouse(player);
+		var netConnect = new NetConnection();
+		netConnect.connect(null);
+		var netStream = new NetStream(netConnect);
+		netStream.client = {
+			onMetaData: function() {
+				player.attachNetStream(netStream);
+				player.width = FlxG.width;
+				player.height = FlxG.height;
 			}
-		}
+		};
+		netConnect.addEventListener(NetStatusEvent.NET_STATUS, function(event:NetStatusEvent) {
+			if(event.info.code == "NetStream.Play.Complete") {
+				netStream.dispose();
+				if(FlxG.game.contains(player)) FlxG.game.removeChild(player);
 
-		bitmap = new VlcBitmap();
-		bitmap.set_height(FlxG.stage.stageHeight);
-		bitmap.set_width(FlxG.stage.stageHeight * (16 / 9));
+				if(finishCallback != null) finishCallback();
+			}
+		});
+		netStream.play(name);
 
-		trace("Setting width to " + FlxG.stage.stageHeight * (16 / 9));
-		trace("Setting height to " + FlxG.stage.stageHeight);
+	        #elseif android
 
-		bitmap.onVideoReady = onVLCVideoReady;
-		bitmap.onComplete = onVLCComplete;
-		bitmap.onError = onVLCError;
+                VideoView.playVideo(AndroidTools.getFileUrl(SUtil.getPath() + name));
+                VideoView.onCompletion = function(){
+		        if (finishCallback != null){
+			        finishCallback();
+		        }
+                }            
 
-		FlxG.stage.addEventListener(Event.ENTER_FRAME, update);
+		#elseif desktop
+		// by Polybius, check out PolyEngine! https://github.com/polybiusproxy/PolyEngine
 
-		if (repeat)
-			bitmap.repeat = -1;
-		else
-			bitmap.repeat = 0;
+		vlcBitmap = new VlcBitmap();
+		vlcBitmap.set_height(FlxG.stage.stageHeight);
+		vlcBitmap.set_width(FlxG.stage.stageHeight * (16 / 9));
 
-		bitmap.inWindow = isWindow;
-		bitmap.fullscreen = isFullscreen;
+		vlcBitmap.onComplete = onVLCComplete;
+		vlcBitmap.onError = onVLCError;
 
-		FlxG.addChildBelowMouse(bitmap);
-		bitmap.play(checkFile(path));
+		FlxG.stage.addEventListener(Event.ENTER_FRAME, fixVolume);
+		vlcBitmap.repeat = 0;
+		vlcBitmap.inWindow = false;
+		vlcBitmap.fullscreen = false;
+		fixVolume(null);
 
-		if (outputTo != null)
-		{
-			// lol this is bad kek
-			bitmap.alpha = 0;
-
-			sprite = outputTo;
-		}
+		FlxG.addChildBelowMouse(vlcBitmap);
+		vlcBitmap.play(checkFile(name));
+		#end
 	}
 
+	#if desktop
 	function checkFile(fileName:String):String
 	{
 		var pDir = "";
@@ -82,85 +91,53 @@ class MP4Handler
 
 		return pDir + fileName;
 	}
+	
+	public static function onFocus() {
+		if(vlcBitmap != null) {
+			vlcBitmap.resume();
+		}
+	}
+	
+	public static function onFocusLost() {
+		if(vlcBitmap != null) {
+			vlcBitmap.pause();
+		}
+	}
 
-	/////////////////////////////////////////////////////////////////////////////////////
-
-	function onVLCVideoReady()
+	function fixVolume(e:Event)
 	{
-		trace("video loaded!");
-
-		if (sprite != null)
-			sprite.loadGraphic(bitmap.bitmapData);
+		// shitty volume fix
+		vlcBitmap.volume = 0;
+		if(!FlxG.sound.muted && FlxG.sound.volume > 0.01) { //Kind of fixes the volume being too low when you decrease it
+			vlcBitmap.volume = FlxG.sound.volume * 0.5 + 0.5;
+		}
 	}
 
 	public function onVLCComplete()
 	{
-		bitmap.stop();
+		vlcBitmap.stop();
 
-		// Clean player, just in case! Actually no.
+		// Clean player, just in case!
+		vlcBitmap.dispose();
 
-		FlxG.camera.fade(FlxColor.BLACK, 0, false);
-
-		trace("Big, Big Chungus, Big Chungus!");
-
-		new FlxTimer().start(0.3, function(tmr:FlxTimer)
+		if (FlxG.game.contains(vlcBitmap))
 		{
-			if (finishCallback != null)
-			{
+			FlxG.game.removeChild(vlcBitmap);
+		}
+
+		if (finishCallback != null)
+		{
+			finishCallback();
+		}
+	}
+
+	
+	function onVLCError()
+		{
+			trace("An error has occured while trying to load the video.\nPlease, check if the file you're loading exists.");
+			if (finishCallback != null) {
 				finishCallback();
 			}
-			else if (stateCallback != null)
-			{
-				LoadingState.loadAndSwitchState(stateCallback);
-			}
-
-			bitmap.dispose();
-
-			if (FlxG.game.contains(bitmap))
-			{
-				FlxG.game.removeChild(bitmap);
-			}
-		});
-	}
-
-	public function kill()
-	{
-		bitmap.stop();
-
-		if (finishCallback != null)
-		{
-			finishCallback();
 		}
-
-		bitmap.visible = false;
-	}
-
-	function onVLCError()
-	{
-		if (finishCallback != null)
-		{
-			finishCallback();
-		}
-		else if (stateCallback != null)
-		{
-			LoadingState.loadAndSwitchState(stateCallback);
-		}
-	}
-
-	function update(e:Event)
-	{
-		if (FlxG.keys.justPressed.ENTER || FlxG.keys.justPressed.SPACE)
-		{
-			if (bitmap.isPlaying)
-			{
-				onVLCComplete();
-			}
-		}
-
-		bitmap.volume = FlxG.sound.volume + 0.3; // shitty volume fix. then make it louder.
-
-		if (FlxG.sound.volume <= 0.1)
-			bitmap.volume = 0;
-	}
-        #end
+	#end
 }
